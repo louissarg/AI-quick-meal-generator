4b08aac5c63fdc68de4a1e750f10a6a7446a2c5e
louissarg <louissarg@gmail.com>
2024-11-25 20:35:43 +0200

    added some Apikey ,apiURL and downloaded axios

--- src/index.js
+++ src/index.js
@@ -1,12 +1,23 @@
+function displayRecipe(response) {
+  new Typewriter("#recipe", {
+    strings: response.data.answer,
+    autoStart: true,
+    delay: 1,
+    cursor: "",
+  });
+}
+
 function generateRecipe(event) {
   event.preventDefault();
 
-  new Typewriter("#recipe", {
-    strings: "step 1 Bring a pan of water to the boil ",
-    autoStart: true,
-    delay: 50,
-    cursor: "",
-  });
+  let instructionsInput = document.querySelector("#user-instructions");
+  let apiKey = "baef1db434f00t5c6510a0f45a35996o";
+  let prompt = `Generate a quick meal recipe ${instructionsInput.value}`;
+  let context =
+    "you are a master chief and you are required to write to generate a quick meal recipe with HTML.";
+  let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  axios.get(apiURL).then(displayRecipe);
 }
 
 let recipeFormElement = document.querySelector("#meal-generator-form");
